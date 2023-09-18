7d4b43051bf71938df2d99802cb54c9a0f6ea3ff
alex <28884101+alex-gitt-hubb@users.noreply.github.com>
2023-09-18 02:25:35 -0400

    fix: Make removeFromArray() pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+/**
+ * @param {any[]} arr 
+ * @param  {...any} args 
+ */
+const removeFromArray = function(arr, ...args) {
+    if(arr.length < 1) return arr; // speed up. unneeded.
 
+    for(let arg of args) {
+        for(let i=0; i<arr.length; ++i) {
+            if(arr[i] === arg)  arr.splice(i, 1)
+        }
+    }
+
+    return arr
 };
 
 // Do not edit below this line
