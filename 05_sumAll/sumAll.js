e031493fe2d428776c7f14507b2dc509db1e8d8c
alex <28884101+alex-gitt-hubb@users.noreply.github.com>
2023-09-18 15:48:50 -0400

    fix: Make sumAll() pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+/**
+ * Sum all numbers in a range [from ~ upTo]
+ * @param {number} from 
+ * @param {number} upTo 
+ */
+const sumAll = function(from, upTo) {
+    if(
+        from < 0 || upTo < 0 ||
+        typeof from != "number" || typeof upTo != "number"
+    )
+        return 'ERROR'
 
+    // allow calls like: sumAll(123, 1)
+    if(from > upTo) {
+        const tmp = from
+        from = upTo
+        upTo = tmp
+    }
+
+    let sum = 0
+    
+    let crnt = from
+    while(crnt <= upTo) {
+        sum += crnt++
+    }
+
+    return sum
 };
 
 // Do not edit below this line
