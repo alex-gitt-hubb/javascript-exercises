4d80c09aa75ba81cdb1ed604e7846c59906e9477
alex <28884101+alex-gitt-hubb@users.noreply.github.com>
2023-09-19 03:16:54 -0400

    fix: Make tempConversion() pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const roundToOneDecimal = num => {
+  return Math.round(num * 10) / 10
+}
+
+/**
+ * @param {number} fahrenheit 
+ */
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * 5/9
+  return roundToOneDecimal(celsius)
 };
 
-const convertToFahrenheit = function() {
+/**
+ * @param {number} celsius 
+ */
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = celsius * 9/5 + 32
+  return roundToOneDecimal(fahrenheit)
 };
 
 // Do not edit below this line
