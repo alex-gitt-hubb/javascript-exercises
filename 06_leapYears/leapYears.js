e69408a91f19e52b04484912ce406307552c32a2
alex <28884101+alex-gitt-hubb@users.noreply.github.com>
2023-09-19 01:46:20 -0400

    fix: Make leapYears() pass all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // leap:
+    //      %4 = 0      &&
+    //     (%100 != 0   ||  %400 == 0)
+    return (
+        (year % 4 == 0) &&
+        (year % 100 != 0 || year % 400 == 0)
+    )
 };
 
 // Do not edit below this line
