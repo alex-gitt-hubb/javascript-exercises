d01a7c5ca82bd6f5577c1b2bbbd342130d4c5779
alex <28884101+alex-gitt-hubb@users.noreply.github.com>
2024-01-13 01:53:10 -0500

    feat(12_findTheOldest): implement logic

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+/**
+ * @param { {name: string; yearOfBirth: number; yearOfDeath: number|undefined;}[] } people
+ */
+const findTheOldest = function(people) {
+  // found
+  let max = {
+    age: -1,
+    i: -1
+  }
 
+  people.forEach((person, i) => {
+    const age = (
+      (person.yearOfDeath || new Date().getFullYear()) // if still alive
+      - person.yearOfBirth
+    )
+
+    if(age > max.age)   max = {age, i}
+  })
+
+  return people[max.i]
 };
 
 // Do not edit below this line
