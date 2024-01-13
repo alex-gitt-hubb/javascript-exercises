6918f11f6ccd5b9dab50c42eca2aea0c783fcb17
alex <28884101+alex-gitt-hubb@users.noreply.github.com>
2024-01-12 23:27:30 -0500

    feat(09_palindromes): implement logic

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,42 @@
-const palindromes = function () {
+/**
+ * helper
+ * @param {string} ch charcter to test
+ * @returns TRUE if the character should be processed
+ */
+function isValidChar(ch) {
+  return (ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9')
+}
 
-};
+/**
+ * @param {string} str string to test
+ * @returns TRUE if 'str' is a palindrome
+ */
+const isPalindrome = function (str) {
+  // ptrs
+  let l = 0
+  let r = str.length - 1
+  
+  while(l < r) {
+    const lChar = str[l]
+    if(!isValidChar(lChar)) {
+      ++l
+      continue
+    }
+    
+    const rChar = str[r]
+    if(!isValidChar(rChar)) {
+      --r
+      continue
+    }
+
+    if(lChar.toLowerCase() !== rChar.toLowerCase()) return false
+
+    ++l
+    --r
+  }
+
+  return true
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = isPalindrome
