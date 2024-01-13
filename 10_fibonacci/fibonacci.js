70025586d8933914710d80e7acc977335b66e254
alex <28884101+alex-gitt-hubb@users.noreply.github.com>
2024-01-13 00:22:41 -0500

    feat(10_fibonacci): implement logic

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+// fibonacci sequence:
+// 0, 1,               1, 2, 3, 5, 8, 13, ..
+// ^  ^                ^  ^  ^  ^  ^  ^
+// defaults (0, 1)     calculated as: m[n-1] + m[n-2]
 
+/**
+ * calculate the num-th fibonacci member
+ * @param {number|string} num 
+ */
+const fibonacci = function(num) {
+  const n = +num
+  if(n < 0) throw new Error("OOPS")
+
+  let m0 = 0
+  let m1 = 1
+
+  for(let i=2; i<=n; i+=2) {
+    m0 = m0 + m1
+    m1 = m0 + m1
+  }
+
+  return n&1 ? m1 : m0
 };
 
 // Do not edit below this line
