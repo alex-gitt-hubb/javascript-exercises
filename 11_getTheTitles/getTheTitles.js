de74f254b9375eac626a61d8b9f9fd15bf2d1bab
alex <28884101+alex-gitt-hubb@users.noreply.github.com>
2024-01-13 01:43:06 -0500

    feat(11_getTheTitles): implement logic

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+/**
+ * @param { {title: string, author: string}[] } books 
+ */
+const getTheTitles = function(books) {
+  return books.map(book => book.title)
 };
 
 // Do not edit below this line
